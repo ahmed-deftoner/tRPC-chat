edbe50b3fb3d1978f501f1db8b39c6eb9b0ddbd7
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-20 17:48:17 +0500

    server errors solved

--- t3-chat/src/server/router/index.ts
+++ t3-chat/src/server/router/index.ts
@@ -2,12 +2,18 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { RoomRouter } from "./room";
+import { roomRouter } from "./room";
 import { authRouter } from "./auth";
+import fetch from "node-fetch";
+
+if (!global.fetch) {
+  (global.fetch as any) = fetch;
+}
+
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("room.", RoomRouter)
+  .merge("room.", roomRouter)
   .merge("auth.", authRouter);
 
 // export type definition of API
