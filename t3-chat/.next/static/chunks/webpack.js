41833196c2cbcfc03ec6697cdb8cf678a6a801e2
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-07-20 17:27:53 +0500

    create session

--- t3-chat/.next/static/chunks/webpack.js
+++ t3-chat/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3f2d814a1e181bf"; }
+/******/ 		__webpack_require__.h = function() { return "ef988979eebb8131"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- t3-chat/src/pages/rooms/[roomId].tsx
+++ t3-chat/src/pages/rooms/[roomId].tsx
@@ -1,8 +1,18 @@
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 function RoomPage(){
     const { query } = useRouter();
     const roomId = query.roomId as string;
+    const { data: session } = useSession();
+
+    if (!session) {
+        return (
+          <div>
+            <button onClick={() => signIn()}>Login</button>
+          </div>
+        );
+      }
 
     return <div>welcome to room {roomId}</div>
 
